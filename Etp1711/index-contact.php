f107e4856c86cb6415f61d303eae46e437784ecf
etp1711 <etp1711@gmail.com>
2013-02-08 19:09:20 +0330

    add inputs and all divs almost contact.php is finish

--- Etp1711/index-contact.php
+++ Etp1711/index-contact.php
@@ -7,8 +7,17 @@
 		</div>
 	</div>
 	<div class="cform">
-		<div class="form"></div>
-		<div class="info"></div>
+		<div class="info">
+			<h2>معلومات الاتصال</h2>
+		</div>
+		<div class="form">
+			<h2>نموذج الاتصال</h2>
+			<input 	class="name" type="input">
+			<input	class="email" type="input">
+			<input	class="subject" type="input">
+			<textarea	class="messege"></textarea>
+			<input class="button" type="button" value="إرسال رسالة">
+		</div>
 		<div class="clear"></div>
 	</div>
 </div>
