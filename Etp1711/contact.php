7fc5f108e6cbc6f2b65d4cd3a909bbccb6de3702
etp1711 <etp1711@gmail.com>
2013-02-08 17:38:27 +0330

    add my files

--- Etp1711/contact.php
+++
@@ -1,8 +0,0 @@
-<?php include('./inc/header.php') ?>
-
-<section class="content">
-	<div class="slidshow"></div>
-	<div class="middle"></div>
-</section>
-
-<?php include('./inc/footer.php') ?>
---
+++ Etp1711/index-contact.php
@@ -0,0 +1,12 @@
+<?php include('./inc/header.php') ?>
+
+<div class="containcnt">
+	<div class="location">
+		
+	</div>
+	<div class="cform">
+		
+	</div>
+</div>
+
+<?php include('./inc/footer.php') ?>
