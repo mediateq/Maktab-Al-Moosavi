e359401a10233d97d72ee1be38bbaa6377234f4b
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-02-08 19:54:48 +0330

    add hline and text in bottom of slideshow

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <section class="content">
 	<div class="slid-part">
 		<div class="slideshow"></div>
+		<div class="hline"></div>
+		<div class="text"><p>هدف ما فقط اقامت شما نیست، هدف ما لذت بردن از اقامت شما می باشد</p></div>
 	</div>
 	<div class="middle"></div>
 </section>
