9679dea2fe717121a22b098193fdac4368bb0fc5
etp1711 <etp1711@gmail.com>
2013-02-08 18:32:44 +0330

    add info and form contact

--- Etp1711/index-contact.php
+++ Etp1711/index-contact.php
@@ -2,10 +2,14 @@
 
 <div class="containcnt">
 	<div class="location">
-		
+		<div class="text"></div>
+		<div class="map">
+		</div>
 	</div>
 	<div class="cform">
-		
+		<div class="form"></div>
+		<div class="info"></div>
+		<div class="clear"></div>
 	</div>
 </div>
 
