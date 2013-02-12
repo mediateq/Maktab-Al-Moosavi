d65864c6379496c0aa19fa509cac15722b6c0f22
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-02-12 11:02:37 +0330

    add images and title to page

--- gallery.php
+++ gallery.php
@@ -1,7 +1,14 @@
 <?php include('./inc/header.php') ?>
 
 <section class="gallery">
-	
+	<div class="title">
+		<h2>گالری تصاویر</h2>
+	</div>
+	<ul>
+		<li><a href="#"><img src="./images/gallery/1.jpg" alt=""></a></li>
+		<li><a href="#"><img src="./images/gallery/2.jpg" alt=""></a></li>
+		<li><a href="#"><img src="./images/gallery/3.jpg" alt=""></a></li>
+	</ul>
 </section>
 
 <?php include('./inc/footer.php') ?>
