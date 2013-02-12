ae9aaf466b5a55b0dad715a2e102259c6e44c7fc
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-02-12 11:42:31 +0330

    add effect to picture

--- gallery.php
+++ gallery.php
@@ -5,10 +5,44 @@
 		<h2>گالری تصاویر</h2>
 	</div>
 	<ul>
-		<li><a href="#"><img src="./images/gallery/1.jpg" alt=""></a></li>
-		<li><a href="#"><img src="./images/gallery/2.jpg" alt=""></a></li>
-		<li><a href="#"><img src="./images/gallery/3.jpg" alt=""></a></li>
+		<li>
+			<a href="#">
+				<img src="./images/gallery/1.jpg" alt="">
+				<div class="effe"></div>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<img src="./images/gallery/2.jpg" alt="">
+				<div class="effe"></div>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<img src="./images/gallery/3.jpg" alt="">
+				<div class="effe"></div>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<img src="./images/gallery/3.jpg" alt="">
+				<div class="effe"></div>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<img src="./images/gallery/2.jpg" alt="">
+				<div class="effe"></div>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<img src="./images/gallery/1.jpg" alt="">
+				<div class="effe"></div>
+			</a>
+		</li>
 	</ul>
+	<div class="badboy"></div>
 </section>
 
 <?php include('./inc/footer.php') ?>
