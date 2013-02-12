51d252dd0e42df89f31e0b9e8ffb301f556c8ec0
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-02-12 16:24:33 +0330

    changes pics

--- gallery.php
+++ gallery.php
@@ -27,32 +27,25 @@
 			<p>عکس شماره یک</p>
 		</li>
 		<li>
-			<a href="./images/gallery/3.jpg" rel='lightbox[group1]' title="عکس شماره چهار">
-				<img src="./images/gallery/3.jpg" alt="">
+			<a href="./images/gallery/4.jpg" rel='lightbox[group1]' title="عکس شماره چهار">
+				<img src="./images/gallery/4.jpg" alt="">
 				<div class="effe"></div>
 			</a>
 			<p>عکس شماره چهار</p>
 		</li>
 		<li>
-			<a href="./images/gallery/2.jpg" rel='lightbox[group1]' title="عکس شماره پنج">
-				<img src="./images/gallery/2.jpg" alt="" >
+			<a href="./images/gallery/5.jpg" rel='lightbox[group1]' title="عکس شماره پنج">
+				<img src="./images/gallery/5.jpg" alt="" >
 				<div class="effe"></div>
 			</a>
 			<p>عکس شماره پنج</p>
 		</li>
 		<li>
-			<a href="./images/gallery/1.jpg" rel='lightbox[group1]' title="عکس شماره شش">
-				<img src="./images/gallery/1.jpg" alt="">
+			<a href="./images/gallery/6.jpg" rel='lightbox[group1]' title="عکس شماره شش">
+				<img src="./images/gallery/6.jpg" alt="">
 				<div class="effe"></div>
 			</a>
 			<p>عکس شماره شش</p>
-		</li>
-		<li>
-			<a href="./images/gallery/1.jpg" rel='lightbox[group1]' title="عکس شماره هفت">
-				<img src="./images/gallery/1.jpg" alt="">
-				<div class="effe"></div>
-			</a>
-			<p>عکس شماره هفت</p>
 		</li>
 	</ul>
 	<div class="badboy"></div>
