5592233bfd9a1a5ebe08a4eb57fd3a3aa499b6e5
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-02-13 20:02:39 +0330

    add pic and text

--- hotels.php
+++ hotels.php
@@ -4,6 +4,84 @@
 	<div class="title">
 		<h2>هتل ها</h2>
 	</div>
+	<ul>
+		<li>
+			<div class="tit"><h2>هتل پنج ستاره قصرطلایی</h2></div>
+			<div class="text">
+				<p>
+					نکات مورد نظر در مورد هتل نکات مورد نظر در مورد هتل نکات مورد نظر در مورد هتل نکات مورد نظر در مورد هتل نکات مورد نظر در مورد هتل نکات مورد نظر در مورد هتل نکات...
+				</p>
+			</div>
+		</li>
+		<li>
+			<a href="#">
+				<img src="./images/hotels/1.jpg" alt="">
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<img src="./images/hotels/2.jpg" alt="">
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<img src="./images/hotels/3.jpg" alt="">
+			</a>
+		</li>
+	</ul>
+	<div class="badboy"></div>
+	<ul>
+		<li>
+			<a href="#">
+				<img src="./images/hotels/4.jpg" alt="">
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<img src="./images/hotels/5.jpg" alt="">
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<img src="./images/hotels/6.jpg" alt="">
+			</a>
+		</li>
+		<li>
+			<div class="tit"><h2>هتل پنج ستاره درویش</h2></div>
+			<div class="text">
+				<p>
+					نکات مورد نظر در مورد هتل نکات مورد نظر در مورد هتل نکات مورد نظر در مورد هتل نکات مورد نظر در مورد هتل نکات مورد نظر در مورد هتل نکات مورد نظر در مورد هتل نکات...
+				</p>
+			</div>
+		</li>
+	</ul>
+	<div class="badboy"></div>
+	<ul>
+		<li>
+			<div class="tit"><h2>هتل پنج ستاره هما</h2></div>
+			<div class="text">
+				<p>
+					نکات مورد نظر در مورد هتل نکات مورد نظر در مورد هتل نکات مورد نظر در مورد هتل نکات مورد نظر در مورد هتل نکات مورد نظر در مورد هتل نکات مورد نظر در مورد هتل نکات...
+				</p>
+			</div>
+		</li>
+		<li>
+			<a href="#">
+				<img src="./images/hotels/7.jpg" alt="">
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<img src="./images/hotels/8.jpg" alt="">
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<img src="./images/hotels/9.jpg" alt="">
+			</a>
+		</li>
+	</ul>
+	<div class="badboy"></div>
 </section>
 
 <?php include('./inc/footer.php') ?>
