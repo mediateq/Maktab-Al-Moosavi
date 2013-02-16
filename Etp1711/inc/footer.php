b3d95df2f46aded11dd508117a8c4ceab9527749
etp1711 <etp1711@gmail.com>
2013-02-16 23:06:18 +0330

    add new footer

--- Etp1711/inc/footer.php
+++ Etp1711/inc/footer.php
@@ -1,6 +1,18 @@
 			<footer>
-				<div class="copy"></div>
-				<div class="about"></div>
+				<div class="copy">
+					<p>
+						کپی رایت 1391 تمامی حقوق این سایت برای المکتب الموسوی رزرو می باشد.
+					</p>
+				</div>
+				<div class="about">
+					<p>
+						درباره شرکت درباره شرکت درباره شرکت درباره شرکت درباره شرکت درباره شرکت درباره شرکت درباره شرکت درباره شرکت درباره شرکت درباره شرکت درباره شرکت درباره شرکت 
+					</p>
+					<p><a href="http://www.mediateq.ir" target="_blank">طراح mediateq</a></p>
+				</div>
+				<div class="anchor">
+					<a href="#myAnchor" id="anchor1" class="anchorLink" title="بالا"></a>
+				</div>
 			</footer>
 		</div>
 	</div>
