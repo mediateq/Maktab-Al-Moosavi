475999cc2590bbc99e1112dac40d8acea3df1c3b
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-02-17 11:58:27 +0330

    add script for mail

--- js/scripts.js
+++ js/scripts.js
@@ -26,6 +26,75 @@ $(document).ready(function() {
 });
 //END Anchor link for scroll
 
+// Begin valid form 
+function validateText(str,len){
+    return str.length >= len;
+}
+
+function validateEmail(str){
+    var emailPattern = /^[a-z0-9+_%.-]+@(?:[a-z0-9-]+\.)+[a-z]{2,6}$/i ;
+
+    return emailPattern.test(str);
+}
+
+(function($){
+    $(function(){
+        $('form#contact').submit(function(){
+            var feild, err=false;
+    
+            target = $('#name');
+    
+            if (validateText( target.val(), 3 )) {
+    
+                target.removeClass('err').addClass('ok');
+    
+            }else{
+    
+                target.removeClass('ok').addClass('err');
+                err= true;
+            }
+    
+            target = $('#mail');
+    
+            if (validateEmail(target.val())) { 
+    
+                target.removeClass('err').addClass('ok');
+            }else {
+    
+                target.removeClass('ok').addClass('err');
+                err=true;
+            }
+
+            target = $('#subject');
+    
+            if (validateText( target.val(), 5 )) {
+    
+                target.removeClass('err').addClass('ok');
+    
+            }else{
+    
+                target.removeClass('ok').addClass('err');
+                err= true;
+            }
+    
+            target = $('#txt');
+    
+            if (validateText( target.val(), 7 )) {
+    
+                target.removeClass('err').addClass('ok');
+    
+            }else {
+    
+                target.removeClass('ok').addClass("err");
+                err=true;
+            }
+    
+            return !err;
+        });
+    });
+})(Zepto);
+// END valid form
+
 //Active menu
 /* (function($){
     $(function(){
