2b117f022800b6ce48d0fc02f890a8e15ca996e5
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-02-17 11:23:52 +0330

    add map

--- contact.php
+++ contact.php
@@ -6,6 +6,7 @@
 	</div>
 	<div class="location">
 		<div class="map">
+			<iframe width="870" height="275" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msa=0&amp;msid=203962002147300705700.0004d5e6c2ae38d716436&amp;hl=en&amp;ie=UTF8&amp;t=m&amp;ll=36.281022,59.611216&amp;spn=0.019027,0.074587&amp;z=14&amp;output=embed"></iframe>
 		</div>
 	</div>
 	<div class="cform">
