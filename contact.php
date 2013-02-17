e5d2665d0cc75c6d3f70995d10e4ff5918b98d4e
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-02-17 11:20:59 +0330

    fix problem in contact

---
+++ contact.php
@@ -0,0 +1,50 @@
+<?php include('./inc/header.php') ?>
+
+<section class="contact">
+	<div class="title">
+		<h2>تماس با ما</h2>
+	</div>
+	<div class="location">
+		<div class="map">
+		</div>
+	</div>
+	<div class="cform">
+		<div class="info">
+			<h2>اطلاعات تماس</h2>
+			<p>
+				اطلاعات تماس در این قسمت قرار میگیرد اطلاعات تماس در این قسمت قرار میگیرد اطلاعات تماس در این قسمت قرار میگیرد اطلاعات تماس در این قسمت قرار میگیرد اطلاعات تماس در این قسمت قرار میگیرد اطلاعات تماس در این قسمت قرار میگیرد اطلاعات تماس در این قسمت قرار میگیرد اطلاعات تماس در این قسمت قرار میگیرد اطلاعات تماس در این قسمت قرار میگیرد اطلاعات تماس در این قسمت قرار میگیرد اطلاعات تماس در این قسمت قرار میگیرد اطلاعات تماس در این قسمت قرار میگیرد 
+			</p>
+			<br />
+			<p>
+				آدرس در این قسمت قرار میگیرد آدرس در این قسمت قرار میگیرد آدرس در این قسمت قرار میگیرد آدرس در این قسمت قرار میگیرد
+			</p>
+			<br />
+			<p>
+				تلفن: 123456789
+			</p>
+			<p>
+				فاکس: 987654321
+			</p>
+			<p>
+				ایمیل: 
+			</p>
+		</div>
+		<div class="form">
+			<h2>فرم تماس</h2>
+			<form action="">
+				<p>نام و نام خانوادگی <span>*</span></p>
+				<input 	class="name" type="input" />
+				<p>ایمیل <span>*</span></p>
+				<input	class="email" type="input" />
+				<p>موضوع <span>*</span></p>
+				<input	class="subject" type="input" />
+				<p>پیام <span>*</span></p>
+				<textarea	class="messege"></textarea>
+				<input class="button" type="submit" value="ارسال" />
+			</form>
+		</div>
+		<div class="badboy"></div>
+	</div>
+</section>
+
+<?php include('./inc/footer.php') ?>
