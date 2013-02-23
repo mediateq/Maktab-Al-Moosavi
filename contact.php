fe8c4201c18463d093be36d5350c5ba8d3087c16
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-02-23 12:41:12 +0330

    off resize lightbox

--- contact.php
+++ contact.php
@@ -56,6 +56,7 @@
   $(document).ready(function(){
     $("a[rel^='prettyphoto']").prettyPhoto({
     	custom_markup: '<iframe width="870" height="580" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msa=0&amp;msid=203962002147300705700.0004d5e6c2ae38d716436&amp;hl=en&amp;ie=UTF8&amp;t=m&amp;ll=36.281022,59.611216&amp;spn=0.019027,0.074587&amp;z=14&amp;output=embed"></iframe>',
+    	allow_resize: false,
     });
   });
 </script>
