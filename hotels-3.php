45ba4d8f92d0b0834c9f1fb1e721eee0ae7516b5
etp1711 <etp1711@gmail.com>
2013-03-03 18:40:33 +0330

    finish hotels

---
+++ hotels-3.php
@@ -0,0 +1,43 @@
+<?php include('./inc/header.php') ?>
+
+<section class="hotels">
+	<div class="title">
+		<h2>الفنادق</h2>
+	</div>
+	<ul>
+		<li>
+			<div class="tit"><h2>خلیج فارس</h2></div>
+			<div class="text">
+				<p>
+					الفندق قصر طلایي الدولي،ثاني فندق من مجموعة فنادق قصر الدولي و هو اکبر فندق في مدینة مشهد المقدسة قد تم بناءه فی سنة 2012 ...
+				</p>
+			</div>
+		</li>
+		<li>
+			<a href="./khalijefars.php">
+				<img src="./images/hotels/ghasrtalae/1.jpg" alt="">
+			</a>
+		</li>
+		<li>
+			<a href="./khalijefars.php">
+				<img src="./images/hotels/ghasrtalae/2.jpg" alt="">
+			</a>
+		</li>
+		<li>
+			<a href="./khalijefars.php">
+				<img src="./images/hotels/ghasrtalae/3.jpg" alt="">
+			</a>
+		</li>
+	</ul>
+	<div class="badboy"></div>
+	
+	<div class="badboy"></div>
+	<div class="but right">
+		<a href="./hotels.php">1</a>
+		<a href="./hotels-2.php">2</a>
+		<a href="./hotels-3.php">3</a>
+	</div>
+	<div class="badboy"></div>
+</section>
+
+<?php include('./inc/footer.php') ?>
---
+++ kiana.php
@@ -0,0 +1,48 @@
+<?php include('./inc/header.php') ?>
+
+<section class="one-hotel">
+	<div class="title">
+		<h2>کیانا</h2>
+	</div>
+	<div class="text">
+		<p>
+			یقع فندق درویشي الفاخر فی شارع الامام رضا(ع) و هو اهم الشوارع الرئیسیة المنتهیة الی الحرم المطهرة ومسافته الی الحرم یعادل1.4کیلومترا .<br />
+الفندق و منذ بدایة بنائه انطلق علی اساس تصمیم فندقي، ویتالف من اثنین و عشرین طابقا لیکون بذلک اکثر الفنادق ارتفاعا و فندق الآتریوم الوحید فی شرق البلاد،یتمتع هذا الفندق بأفضل معاییر جودة البناء و قد حاز علی المرتبة الأولی فی مجالات الانشاء و العمارة و بناء المدن فی مهرجان البناء الأفضل في ایران.<br /><br />
+لقد عمدت ادارة الفندق علی اتخاذ الکثیر من الاجراءات و الأعمال التی تهدف الی توفیر الراحة التامة للضیوف و الزوار وتمکنهم من الحصول علی الخدمات التی یحتاجونها بلا ای عناء مع خیارات کثیرة فی نوع الخدمات و غرف الاقامة،وبالنتیجة اقامة لا تمحی من البال فی فندق درویشي الفاخر.
+
+		</p>
+	</div>
+	<div class="pics">
+		<ul>
+			<li>
+				<a href="./images/hotels/darvishi/1.jpg" rel='prettyphoto[gallery1]'>
+					<img src="./images/hotels/darvishi/1.jpg" alt="">
+					<div class="effe"></div>
+				</a>
+			</li>
+			<li>
+				<a href="./images/hotels/darvishi/2.jpg" rel='prettyphoto[gallery1]'>
+					<img src="./images/hotels/darvishi/2.jpg" alt="">
+					<div class="effe"></div>
+				</a>
+			</li>
+			<li>
+				<a href="./images/hotels/darvishi/3.jpg" rel='prettyphoto[gallery1]'>
+					<img src="./images/hotels/darvishi/3.jpg" alt="">
+					<div class="effe"></div>
+				</a>
+			</li>
+		</ul>
+		<div class="badboy"></div>
+	</div>
+</section>
+
+<script type="text/javascript" charset="utf-8">
+  $(document).ready(function(){
+    $("a[rel^='prettyphoto']").prettyPhoto({
+    	autoplay_slideshow: true,
+    });
+  });
+</script>
+
+<?php include('./inc/footer.php') ?>
