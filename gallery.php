40d369c6a2027ae4853d59640884514d1e78c769
etp1711 <etp1711@gmail.com>
2013-03-07 20:26:24 +0330

    add gallery page 2

--- gallery.php
+++ gallery.php
@@ -6,70 +6,75 @@
 	</div>
 	<ul>
 		<li>
-			<a href="./images/gallery/1.jpg" rel="prettyphoto[gallery1]" title="حرم الامام رضا (ع)">
-				<img src="./images/gallery/1.jpg" alt="">
+			<a href="./images/gallery/gallery1/1.jpg" rel="prettyphoto[gallery1]" title="حرم الامام رضا (ع)">
+				<img src="./images/gallery/gallery1/1.jpg" alt="">
 				<div class="effe"></div>
 			</a>
 			<p>حرم الامام رضا (ع)</p>
 		</li>
 		<li>
-			<a href="./images/gallery/2.jpg" rel='prettyphoto[gallery1]' title="متحف و مرقد الشاعر الایراني فرىوسي">
-				<img src="./images/gallery/2.jpg" alt="">
+			<a href="./images/gallery/gallery1/2.jpg" rel='prettyphoto[gallery1]' title="متحف و مرقد الشاعر الایراني فرىوسي">
+				<img src="./images/gallery/gallery1/2.jpg" alt="">
 				<div class="effe"></div>
 			</a>
 			<p>متحف و مرقد الشاعر الایراني فرىوسي</p>
 		</li>
 		<li>
-			<a href="./images/gallery/3.jpg" rel='prettyphoto[gallery1]' title="مسجد جوهرشاد">
-				<img src="./images/gallery/3.jpg" alt="">
+			<a href="./images/gallery/gallery1/3.jpg" rel='prettyphoto[gallery1]' title="مسجد جوهرشاد">
+				<img src="./images/gallery/gallery1/3.jpg" alt="">
 				<div class="effe"></div>
 			</a>
 			<p>مسجد جوهرشاد</p>
 		</li>
 		<li>
-			<a href="./images/gallery/4.jpg" rel='prettyphoto[gallery1]' title="حرم الامام رضا (ع)">
-				<img src="./images/gallery/4.jpg" alt="">
+			<a href="./images/gallery/gallery1/4.jpg" rel='prettyphoto[gallery1]' title="حرم الامام رضا (ع)">
+				<img src="./images/gallery/gallery1/4.jpg" alt="">
 				<div class="effe"></div>
 			</a>
 			<p>حرم الامام رضا (ع)</p>
 		</li>
 		<li>
-			<a href="./images/gallery/5.jpg" rel='prettyphoto[gallery1]' title="متحف الملك نادر">
-				<img src="./images/gallery/5.jpg" alt="" >
+			<a href="./images/gallery/gallery1/5.jpg" rel='prettyphoto[gallery1]' title="متحف الملك نادر">
+				<img src="./images/gallery/gallery1/5.jpg" alt="" >
 				<div class="effe"></div>
 			</a>
 			<p>متحف الملك نادر</p>
 		</li>
 		<li>
-			<a href="./images/gallery/6.jpg" rel='prettyphoto[gallery1]' title="سرزمین موجهای آبی مشهد">
-				<img src="./images/gallery/6.jpg" alt="">
+			<a href="./images/gallery/gallery1/6.jpg" rel='prettyphoto[gallery1]' title="سرزمین موجهای آبی مشهد">
+				<img src="./images/gallery/gallery1/6.jpg" alt="">
 				<div class="effe"></div>
 			</a>
 			<p>سرزمین موجهای آبی مشهد</p>
 		</li>
 		<li>
-			<a href="./images/gallery/7.jpg" rel='prettyphoto[gallery1]' title="جالیدره">
-				<img src="./images/gallery/7.jpg" alt="">
+			<a href="./images/gallery/gallery1/7.jpg" rel='prettyphoto[gallery1]' title="جالیدره">
+				<img src="./images/gallery/gallery1/7.jpg" alt="">
 				<div class="effe"></div>
 			</a>
 			<p>جالیدره</p>
 		</li>
 		<li>
-			<a href="./images/gallery/8.jpg" rel='prettyphoto[gallery1]' title="ریش شاندیز">
-				<img src="./images/gallery/8.jpg" alt="">
+			<a href="./images/gallery/gallery1/8.jpg" rel='prettyphoto[gallery1]' title="ریش شاندیز">
+				<img src="./images/gallery/gallery1/8.jpg" alt="">
 				<div class="effe"></div>
 			</a>
 			<p>ریش شاندیز</p>
 		</li>
 		<li>
-			<a href="./images/gallery/9.jpg" rel='prettyphoto[gallery1]' title="مجمع الماس">
-				<img src="./images/gallery/9.jpg" alt="">
+			<a href="./images/gallery/gallery1/9.jpg" rel='prettyphoto[gallery1]' title="مجمع الماس">
+				<img src="./images/gallery/gallery1/9.jpg" alt="">
 				<div class="effe"></div>
 			</a>
 			<p>مجمع الماس</p>
 		</li>
 	</ul>
 	<div class="badboy"></div>
+	<div class="but right">
+		<a href="./gallery.php">1</a>
+		<a href="./gallery-2.php">2</a>
+	</div>
+	<div class="badboy"></div><br />
 </section>
 
 <script type="text/javascript" charset="utf-8">
