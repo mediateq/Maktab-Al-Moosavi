fff3c328fa3c2984a91a794a54a8083983bd63cd
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-03-21 02:23:04 +0330

    add counter

--- inc/footer.php
+++ inc/footer.php
@@ -9,6 +9,17 @@
 						هدف ما فقط اقامت شما نیست، هدف ما لذت بردن شما از اقامتتان می باشد
 					</p>
 					<p><a href="http://www.mediateq.ir" target="_blank">Designed by mediateq</a></p>
+					<div class="amar">
+						<div style="display:none;">
+							<script type="text/javascript" src="http://counter.toolsir.com/secure.js"></script>
+							<script type="text/javascript">
+							  var _torder = '&pic=1';
+							  var _turl = 'http://www.maktabalmosawi.ir';
+							  _tCounter (_torder,_turl);
+							</script>
+						</div>
+						<a href="http://www.toolsir.com/counter/url/maktabalmosawi.ir" target="_blank"><img src="./images/chart.gif" alt="شمارنده" width="19" height="16" /></a>
+					</div>
 				</div>
 				<div class="anchor">
 					<a href="#myAnchor" id="anchor1" class="anchorLink" title="أعلى"></a>
